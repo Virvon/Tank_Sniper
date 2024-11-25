51edfa30401879044af702ed19fb157d0b968798
Virvon <dan130205@gmail.com>
2024-11-25 12:01:52 +0300

    Add InputService

---
+++ Assets/Sources/Services/InputService/InputService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Sources.Services.InputService
+{
+    public class InputService : IDisposable
+    {
+        private readonly InputActionSheme _inputActionSheme;
+
+        public InputService()
+        {
+            _inputActionSheme = new();
+
+            _inputActionSheme.Enable();
+
+            _inputActionSheme.PlayerInput.Aiming.performed += ctx => Debug.Log(ctx);
+        }
+
+        public void Dispose()
+        {
+            Debug.Log("dispose input");
+        }
+    }
+}
